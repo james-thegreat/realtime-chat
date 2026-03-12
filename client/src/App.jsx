fe72d64a086f781f5c97a00e579ae1f05fcd9005
James Dunlop <jamezdunlop@gmail.com>
2026-03-12 13:18:06 +1300

    Created the file stucture for this project.

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Real-Time Chat</h1>
+      <p>Frontend setup is working.</p>
+    </div>
+  );
+}
+
+export default App;
